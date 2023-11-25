ee98ea11a5726e7f77562278f2d2ccab6554ec7c
Jon-Felipe <rodriguesjf7@gmail.com>
2023-11-25 18:17:53 +0000

    get all users route functionality

--- controller/userController.js
+++ controller/userController.js
@@ -1,18 +1,19 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 
-// @desc    Get all users
-// @route   GET /api/users
-// @access  Private/Admin
-const getUsers = asyncHandler(async (req, res) => {
-  res.send('get users');
-});
-
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   res.send('auth user');
+});
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.status(200).json({ users });
 });
 
 export { authUser, getUsers };
