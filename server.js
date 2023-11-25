31e94d953ab64dfb424b73e4e67f15446e62449f
Jon-Felipe <rodriguesjf7@gmail.com>
2023-11-25 08:56:55 +0000

    basic user auth route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
 
 connectDB();
 
@@ -11,6 +12,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
