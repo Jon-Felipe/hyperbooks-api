1d7a5019ee3d3bcf916acfba8299f7eced3fb4c1
Jon-Felipe <rodriguesjf7@gmail.com>
2023-11-26 17:18:06 +0000

    match password functionality

--- controller/userController.js
+++ controller/userController.js
@@ -7,7 +7,7 @@ import User from '../models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  if (user) {
+  if (user && (await user.matchPassword(password))) {
     res.status(200).json({
       _id: user._id,
       name: user.name,
